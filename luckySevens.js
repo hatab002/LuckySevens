d3b167bc8f0962f449d629d6ba16b82498b5a6ea
hatab002 <hatab002@umn.edu>
2019-01-18 22:10:22 -0600

    javascript working for most part

--- luckySevens.js
+++ luckySevens.js
@@ -1,27 +1,40 @@
 
-var play = function play(){
+var play = function (){
 var gameMoney = document.getElementById("startingBet").value
-var diceOne = Math.floor(Math.random * 6 + 1)
-var diceTwo = Math.floor(Math.random * 6 + 1)
-var diceRolled = []
-var diceSum = 0
-var money = []
+var money = [parseInt(gameMoney)]
 var moneyTotal= 0
-if (gameMoney == NaN || gameMoney <= 0){
-    alert ("Please enter a bet that is greater than zero")
-} else if (gameMoney >0){
-    console.log(gameMoney)
-    diceRolled.push(diceOne);
-    diceRolled.push(diceTwo);
-    diceSum = diceRolled[0] + diceRolled[1]
-    if (diceSum == 7){
-        gameMoney += 4
-    } else { gameMoney -=1
-    }
-    money.push([gameMoney, diceRolled.length/2])
-} else if ( gameMoney <= 0){
-    alert("gameOver");
-    document.getElementById("resultsSection").style("display:block")
+var mostMoney = 0
+var diceSum = 0
+if (gameMoney == NaN || gameMoney <= 0) {
+    alert("Please enter a bet greater than zero")
 }
+while(gameMoney > 0){
+    var diceOne =   Math.floor(Math.random() * 6) + 1
+    console.log(diceOne)
+    var diceTwo = Math.floor(Math.random() * 6) + 1
+    console.log(diceTwo)
+    // for (j = 0; j < 1000; j++){
+    // diceSum = Math.floor(Math.random * 6 + 1) + Math.floor(Math.random * 6 + 1)
+   diceSum = diceOne + diceTwo
+   console.log( "dice sum" + diceSum)
+    if (diceSum == 7){
+        // console.log(diceSum)
+    gameMoney += 4;
+    console.log(" game money:" + gameMoney)
+} else {
+    // console.log(diceSum)
+
+    gameMoney--;
+    console.log(" game money:" + gameMoney)
+}
+money.push(gameMoney)
+console.log(money)
+for (k = 0; k < money.length; k++){
+    if (money[k] > mostMoney){
+        mostMoney = money[k]
+    }
+}
+console.log("Rolls at max money" + money.indexOf(mostMoney))
 }
 
+}
