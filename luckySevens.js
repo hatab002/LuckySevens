32bf004945723c03101955357e9cb1600098debb
hatab002 <hatab002@umn.edu>
2019-01-20 17:37:03 -0600

    final version

--- luckySevens.js
+++ luckySevens.js
@@ -1,7 +1,8 @@
 
 var play = function (){
-var gameMoney = document.getElementById("startingBet").value
-var money = [parseInt(gameMoney)]
+document.getElementById("jumbo").style.display = "none";
+var gameMoney = parseFloat(document.getElementById("startingBet").value)
+var money = [gameMoney]
 var mostMoney = 0
 var diceSum = 0
 if (gameMoney == NaN || gameMoney <= 0) {
@@ -10,37 +11,23 @@ if (gameMoney == NaN || gameMoney <= 0) {
 }
 while(gameMoney > 0){
     var diceOne =   Math.floor(Math.random() * 6) + 1
-    console.log(diceOne)
     var diceTwo = Math.floor(Math.random() * 6) + 1
-    console.log(diceTwo)
-    // for (j = 0; j < 1000; j++){
-    // diceSum = Math.floor(Math.random * 6 + 1) + Math.floor(Math.random * 6 + 1)
    diceSum = diceOne + diceTwo
-   console.log( "dice sum" + diceSum)
     if (diceSum == 7){
-        // console.log(diceSum)
     gameMoney += 4;
-    console.log(" game money:" + gameMoney)
 } else {
-    // console.log(diceSum)
-
     gameMoney--;
-    console.log(" game money:" + gameMoney)
 }
 money.push(gameMoney)
-console.log(money)
 for (k = 0; k < money.length; k++){
     if (money[k] > mostMoney){
         mostMoney = money[k]
     }
 }
-console.log("Rolls at max money" + money.indexOf(mostMoney))
-console.log("Total Rolls: " + (money.length - 1))
-console.log("most money" + mostMoney)
 document.getElementById("startingBet").value = gameMoney
 }
 
-if (gameMoney == 0){
+if (gameMoney <= 0){
     document.getElementById("resultsSection").style.display = "block";
     document.getElementById("startBet").value = "$" + money[0];
     document.getElementById("totalRolls").value = money.length -1;
@@ -58,4 +45,5 @@ var reset = function () {
     document.getElementById("startBet").value = 0;
     document.getElementById("maxMoney").value = 0;
     document.getElementById("rollsMax").value = 0;
+    document.getElementById("jumbo").style.display = "block";
 }
